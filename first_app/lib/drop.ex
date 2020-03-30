a820f6a9827be84b87c1783054ab730d6f5a3912
Edgard Petremann <epetremann@gmail.com>
2020-03-31 06:53:25 +0900

    2020-03-31

--- first_app/lib/drop.ex
+++ first_app/lib/drop.ex
@@ -1,7 +1,37 @@
 defmodule Drop do
+  @moduledoc """
+  Functions calculating velocities achieved by objects dropped in a vacuum.
 
-  def fall_velocity(distance) do
-    :math.sqrt(2*9.8 * distance)
+  From *Introducing Elixir, Second Edition, O'Reilly Media, Inc., 2017.
+  Copyright 2017 by Simon St. Laurent and J. David Eisenberg.
+
+  """
+  @doc """
+  Calculates the velocity of an object falling on earth (or on another planet)
+  as if it were in a vacuum (no air resistance). The distance is
+  the height from which the object falls, specified in meters,
+  and the function returns a velocity in meters per second.
+  """
+
+  @spec fall_velocity(atom(), number()) :: float()
+  def fall_velocity(planet \\ :earth, distance ) when is_atom(planet) and  distance >= 0 do
+    # returns speed of falling object after a certain distance in meter per second
+    import :math, only: [sqrt: 1]     # import the function sqrt from erlang mosule :math
+    sqrt(2 * gravity(planet) * distance)
   end
+
+  @doc """
+  Get the gravity of a planet.
+  returns 0 is planet is unknown.
+  """
+  @spec gravity(atom()) :: float
+  def gravity(planet) do
+    %{
+    earth: 9.8 ,
+    mars:  3.71,
+    moon:  1.6
+  } |> Map.get(planet, 0)  #get gravity of planet, 0 if not in the map
+  end
+
 
 end
--- first_app/test/drop_test.exs
+++ first_app/test/drop_test.exs
@@ -1,4 +1,3 @@
-
 defmodule DropTest do
   use ExUnit.Case
   doctest Drop
@@ -7,6 +6,16 @@ defmodule DropTest do
     assert Drop.fall_velocity(20) == 19.79898987322333
   end
   test "mps_to_mph(test fall_velocity(20))" do
-    Convert.mps_to_mph(Drop.fall_velocity(20)) == 44.289078952755766
+    assert Convert.mps_to_mph(Drop.fall_velocity(20)) == 44.289078952755766
+  end
+  test "test fall_velocity/2)" do
+    assert Drop.fall_velocity(20) == 19.79898987322333
+    assert Drop.fall_velocity(:earth, 20) == 19.79898987322333
+    assert Drop.fall_velocity(:moon, 20) == 8.0
+    assert Drop.fall_velocity(:mars, 20) == 12.181953866272849
+    assert Drop.fall_velocity(:jupiter, 20) == 0
   end
 end
+
+
+
